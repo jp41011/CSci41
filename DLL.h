333b27f76f010888f1c7e3456ecd6e2fc57e5e44
Juan <jp41011@gmail.com>
2014-03-15 23:34:02 -0700

    Finished Project 1. Got everything to work.

--- DLL.h
+++ DLL.h
@@ -23,16 +23,20 @@ public:
 	
 	void print(){
 		NodeD* it = front;
+		int count = 0;
 		cout << "Dlist: ";
 		while(it != NULL)
 		{
 			cout << it->data << " ";
 			it = it->next;
+			count++;
 		}
 		cout << endl;
+		//cout << "Nodes: " << count << endl; // for testing
 		return;
 	}
-	// TODO fix insertInOrder
+
+	// Insert a character in order to the list
 	void insertInOrder(char ch){
 		char ch2 = toupper(ch);
 		if(front == NULL) // empty list
@@ -43,21 +47,73 @@ public:
 		NodeD* it = front;
 		while(it != NULL)
 		{
-			if(ch2 < it->data){
+			if(ch2 <= it->data){
 				NodeD* newNode = new NodeD(ch2, it->prev, it);
-				it->prev = newNode;
-				newNode->prev->next = newNode;
+				// error here it->prev when null
+				if(it->prev != NULL) // if not front
+					newNode->prev->next = newNode;
+				else // it was front
+				{	
+
+					front = newNode; // update new front
+				}
+				it->prev = newNode; // update it's prev to new node
 				return;
-			}else if(it->next == NULL){
+			}else if(it == tail){ //->next == NULL){ // if it is tail
 				NodeD* newNode = new NodeD(ch2,it, NULL);
 				it->next = newNode;
 				tail = newNode;
 				return;
-			}
+			}else
+				it = it->next;
 		}
 	}
-	//void deleteKthNode(int k)
-	//void shiftKNodes(int k)
+	// delete kth node from the begining starting at 0
+	void deleteKthNode(int k){
+		NodeD* it = front;
+		bool error = false;
+		for(int i=0; i<k; i++){
+			if(it == NULL)
+			{
+				error = true;
+				break;
+			}else
+				it = it->next;
+		}
+		if(error==true)
+			return;
+		//else
+		//	cout << "Found: " << it->data << endl; // testing
+		NodeD* p = it->prev;
+		NodeD* n = it->next;
+		if(p != NULL)
+			p->next = n;
+		if(n != NULL)
+			n->prev = p;
+		if(it == front)
+			front = n;
+		if(it == tail)
+			tail = p;
+		delete it;
+	}
+	// shift k nodes to the left (circular)
+	void shiftKNodes(int k){
+		// first make list circular
+		if(front == NULL) // empty list
+			return; //exit
+		front->prev = tail;
+		tail->next = front;
+		// then shift front and tail to the left k times
+		for(int i=0; i<k; i++)
+		{
+			front = front->next;
+			tail = tail->next;
+		}
+		//unlink ends of the list
+		front->prev = NULL;
+		tail->next = NULL;
+		return;
+	}
 
 };
 
--- project1.cpp
+++ project1.cpp
@@ -9,7 +9,9 @@ using namespace std;
 int main()
 {
 	// part 1 - Singly Linked List
-	/*
+	cout << "Juan Pedraza CSCI 41" << endl;
+	cout << "--------- Part 1 ---------" << endl;
+	
 	SLL list1;
 	list1.print();
 	list1.pushBack('A');
@@ -35,21 +37,33 @@ int main()
 	cout << list1.findkthNodeFromBack(6) << endl;
 	cout << "1st from back: ";
 	cout << list1.findkthNodeFromBack(1) << endl;
-	*/
-	// part 2 - Doubly Linked List
-	DLL list2;
-	list2.insertInOrder('c');
-	list2.insertInOrder('a');
-	//list2.insertInOrder('a');
-	//list2.insertInOrder('e');
-	//list2.insertInOrder('d');
-	list2.print();
 
+	// part 2 - Doubly Linked List
+	cout << "--------- Part 2 ---------" << endl;
+	DLL list2;
+	list2.insertInOrder('c'); // insert in unsorted order
+	list2.insertInOrder('b');
+	list2.insertInOrder('a');
+	list2.insertInOrder('a');
+	list2.insertInOrder('c');
+	list2.insertInOrder('d');
+	list2.insertInOrder('f');
+	list2.insertInOrder('e');
+	list2.print();
+	cout << "Delete 1th node" << endl << "Delete 3th node" << endl;
+	list2.deleteKthNode(1); // remove 2nd a
+	list2.deleteKthNode(3); // remove 2nd c
+	list2.print();
+	cout << "Shift 2 spaces to the left" << endl;
+	list2.shiftKNodes(2); // shift 2 spaces to the left
+	list2.print();
+	/*
 	char c, d = 'D';
 	cin >> c;
 	if(c > d)
 		cout << "Greater" << endl;
 	else
 		cout << "Less" << endl;
+	*/
 	return 0;
 }
