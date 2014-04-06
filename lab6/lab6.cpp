8c2806198d314b5328fe89422e901a4d0514416c
JP <jp41011@gmail.com>
2014-04-06 02:49:44 -0700

    implemented selections sort. need to implement insertion sort and comment code.

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -12,12 +12,20 @@ int main()
 	cin >> elements;
 	vector<short int> vec0(elements); // create vector w/ given number of elements
 	for(unsigned short i=0; i<elements; i++) // fill vector w/ random values
-		vec0.push_back((rand()%200 - 100)); //random number -100 to 100
+		vec0[i] = rand()%200 - 100; // random number -100 to 100
+
+	cout << "Random numbers: ";
+	cout << vec0; // print out vector using overloaded operator
 	vector<short int> vec1(vec0); // copy of vector vec0
 	vector<short int> vec2(vec0); // copy vec0
-
 	selectionSort(vec1);
+	cout << vec1;
 	insertionSort(vec2);
 
+	char ch;
+	cout << "Redo: r\tExit: e" << endl;
+	cin >> ch;
+	if(ch == 'r')
+		main();
 	return 0;
 }
--- lab6/sorts.cpp
+++ lab6/sorts.cpp
@@ -2,12 +2,44 @@
 #include <vector>
 using namespace std;
 
+void xchg(short& a, short& b)
+{
+	short temp = a;
+	a = b;
+	b = temp;
+	return;
+}
+
 void selectionSort(vector<short int> & vec)
 {
-	cout << "selections sort" << endl;
+	cout << "Selections Sort" << endl;
+	short size = vec.size();
+	short minIndex = 0;
+	for(short i=0; i<size; i++)
+	{
+		minIndex = i;
+		for(short j=i+1; j<size; j++)
+		{
+			if(vec[j] < vec[minIndex])
+				minIndex = j;
+		}
+		if(i != minIndex)
+			xchg(vec[i], vec[minIndex]); //swap elements
+	}
+	return;
 }
 
 void insertionSort(vector<short> & vec)
 {
 	cout << "insertion sort" << endl;
+}
+
+ostream& operator << (ostream& out, vector<short>& vec)
+{
+	short size = vec.size();
+	for(short i=0; i<size; i++)
+	{
+		out << vec[i] << " ";
+	}
+	out << endl;
 }
