bcef69db5341f9dcc14a45de6d0fcbe19f06026d
JP <jp41011@gmail.com>
2014-04-12 20:51:57 -0700

    Started lab7. Need to implement Merge Sort and Quick Sort

---
+++ lab7/lab7.cpp
@@ -0,0 +1,47 @@
+// Juan Pedraza
+/* CSCI 41 - Lab7
+Implementation of merge sort and quick sort
+*/
+#include <iostream>
+#include <stdlib.h> // rand()
+#include <vector>
+#include "sorts.cpp"
+using namespace std;
+
+// overloading the << operator to print out vectors
+ostream& operator << (ostream& out, vector<short>& vec)
+{
+	short size = vec.size();
+	for(short i=0; i<size; i++)
+		out << vec[i] << " ";
+	out << endl;
+}
+
+int main()
+{
+	unsigned short nums = 0;
+	cout << "How many random numbers: ";
+	cin >> nums;
+	vector<short int> vec0 (0);
+	for(unsigned short i=0; i<nums; i++)
+		vec0.push_back(rand()%2000 - 1000);
+
+	cout << "Random numbers: ";
+	cout << vec0;
+	//copy vectors to sort with merge and quick sort
+	vector<short> vec1(vec0);
+	vector<short> vec2(vec0);
+	cout << "Merge Sort: ";
+	mergeSort(vec1);
+	cout << vec1;
+	cout << "Quick Sort: ";
+	quickSort(vec2);
+	cout << vec2;
+	char ch;
+	cout << "Redo: r\tExit: e" << endl;
+	cin >> ch;
+	if(ch == 'r')
+		main();
+
+	return 0;
+}
---
+++ lab7/sorts.cpp
@@ -0,0 +1,25 @@
+// Implement Merge Sort and Quick Sort
+#include <vector>
+using namespace std;
+
+/*function xchg - takes in 2 references to short integers and will
+swap the values. 
+*/
+void xchg(short& a, short & b)
+{
+	short temp = a;
+	a = b;
+	b = temp;
+	return;
+}
+
+// Merge Sort
+void mergeSort(vector<short>& vec)
+{
+
+}
+// Quick Sort
+void quickSort(vector<short>& vec)
+{
+	
+}
