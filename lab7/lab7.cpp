b72a5a7e4f5aa308ff070fda0959e92bc8ba6fd7
JP <jp41011@gmail.com>
2014-04-13 04:04:11 -0700

    Merge Sort not working correctly, need to fix bugs.

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -4,6 +4,7 @@ Implementation of merge sort and quick sort
 */
 #include <iostream>
 #include <stdlib.h> // rand()
+#include <time.h> // for srand(time(NULL))
 #include <vector>
 #include "sorts.cpp"
 using namespace std;
@@ -19,10 +20,12 @@ ostream& operator << (ostream& out, vector<short>& vec)
 
 int main()
 {
+	cout << "time: " << time(NULL) << endl;
 	unsigned short nums = 0;
 	cout << "How many random numbers: ";
 	cin >> nums;
 	vector<short int> vec0 (0);
+	srand(time(NULL));
 	for(unsigned short i=0; i<nums; i++)
 		vec0.push_back(rand()%2000 - 1000);
 
--- lab7/sorts.cpp
+++ lab7/sorts.cpp
@@ -14,10 +14,55 @@ void xchg(short& a, short & b)
 }
 
 // Merge Sort
+void mergeSortHelper(vector<short>&, short, short); // declarring functions
+void merge(vector<short>&, short, short, short); // will define them later
+
 void mergeSort(vector<short>& vec)
 {
-
+	mergeSortHelper(vec, 0, vec.size());
 }
+
+void mergeSortHelper(vector<short>& vec, short low, short high)
+{
+	if(low < high)
+	{
+		short mid = (low+high)/2;
+		mergeSortHelper(vec, low, mid);
+		mergeSortHelper(vec, mid+1, high);
+		merge(vec, low, mid, high);
+	}
+}
+
+void merge(vector<short> & vec, short low, short mid, short high)
+{
+	short leftIndex = low; // index for the left array
+	short rightIndex = mid; // index for the right array
+	vector<short> tempVec (0);
+
+	while(leftIndex < mid && rightIndex <= high)
+	{
+		if(vec[leftIndex] < vec[rightIndex])
+		{
+			tempVec.push_back(vec[leftIndex]);
+			leftIndex++;
+		}else{
+			tempVec.push_back(vec[rightIndex]);
+			rightIndex++;
+		}
+	}
+	while(leftIndex < mid)
+	{
+		tempVec.push_back(vec[leftIndex++]);
+	}
+	while(rightIndex <= high){
+		tempVec.push_back(vec[rightIndex++]);
+	}
+
+	short vecIndex = low;
+	for(unsigned short i=0; i<tempVec.size(); i++)
+		vec[vecIndex++] = tempVec[i];
+}
+
 // Quick Sort
 void quickSort(vector<short>& vec)
 {
